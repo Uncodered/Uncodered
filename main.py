1f5aacfcda2bf285e452ef4f63c93bd1fd15c73e
Uncodered <86816187+Uncodered@users.noreply.github.com>
2021-07-02 12:21:55 +0530

    Update main.py
    Start pattern

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import time
 # author: Uncodered
 # python
-print("Star pattern\n")
-one = int(input("Enter how many rows do you want to print\n"))
-two = bool(int(input("Enter 1 or 0\n")))
-if two == True:
-	for i in range (0, one+1):
-		print("*" * i)
-elif two == False:
-	for i in range (one, 0, -1):
-		print("*" * i)
+try:
+	print("Star pattern\n")
+	one = int(input("Enter how many rows do you want to print\n"))
+	two = bool(int(input("Enter 1 or 0\n")))
+	print("\nloading...")
+	time.sleep(2)
+	if two == True:
+		for i in range (0, one+1):
+			print("*" * i)
+	elif two == False:
+		for i in range (one, 0, -1):
+			print("*" * i)
+	else:
+		print("Error")
+	print("\ndone\n")
+except:
+	print("Error 404")
+print("made by uncodered")
